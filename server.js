53f03036cf07129636e74eaf8d945b5bf25032b8
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-28 12:44:51 -0700

    set up basic server with socket.io; not functional yet

---
+++ server.js
@@ -0,0 +1,13 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`)
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected...')
+});
